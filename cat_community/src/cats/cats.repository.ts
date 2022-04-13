ec4feb7e1f54c119a8dab1a05223d7f130d3a86d
Jin-Mook <jink0608@naver.com>
2022-04-14 08:43:39 +0900

    repository 패턴과 레이어 분리

---
+++ cat_community/src/cats/cats.repository.ts
@@ -0,0 +1,23 @@
+import { CatRequestDto } from './dto/cats.request.dto';
+import { Injectable, HttpException } from '@nestjs/common';
+import { InjectModel } from '@nestjs/mongoose';
+import { Cat } from './cats.schema';
+import { Model } from 'mongoose';
+
+@Injectable()
+export class CatsRepository {
+  constructor(@InjectModel(Cat.name) private readonly catModel: Model<Cat>) { }
+
+  async existsByEmail(email: string): Promise<any> {
+    try {
+      const result = await this.catModel.exists({ email });
+      return result;
+    } catch (error) {
+      throw new HttpException('db error', 400);
+    }
+  }
+
+  async create(cat: CatRequestDto): Promise<Cat> {
+    return await this.catModel.create(cat);
+  }
+}
